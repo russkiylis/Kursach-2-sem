683a31c79bab358e7d5f5102d12e96b44ed627d6
Кощеев Михаил <koshcheev@hotmail.com>
2024-02-17 19:09:31 +0300

    Добавьте файлы проекта.

---
+++ Kursach.h
@@ -0,0 +1,32 @@
+﻿
+// Kursach.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CKursachApp:
+// Сведения о реализации этого класса: Kursach.cpp
+//
+
+class CKursachApp : public CWinApp
+{
+public:
+	CKursachApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CKursachApp theApp;
