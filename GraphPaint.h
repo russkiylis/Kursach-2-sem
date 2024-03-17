4db4500f95fb65b808624e7ba54a939589f0b3a0
Кощеев Михаил <koshcheev@hotmail.com>
2024-03-17 21:09:25 +0300

    микро чё нибудь

--- GraphPaint.h
+++ GraphPaint.h
@@ -34,35 +34,3 @@ public:
 };
 
 
-
-//// Класс рисования ДПФ
-//class CDPFPaint :
-//    public CStatic
-//{
-//    DECLARE_DYNAMIC(CDPFPaint)  // Объявление класса динамическим (для того чобы впихивать переменные)
-//public:
-//    //Переменные
-//
-//    int N;
-//    int G;
-//    int F;
-//    int Fm;
-//    int M;
-//    int XScale;
-//    int YScale;
-//    bool IsLog;
-//
-//    COLORREF GColor, CColor, BColor;
-//    int GWidth;
-//    int CXDensity, CYDensity;
-//
-//    // Объявление карты событий
-//    DECLARE_MESSAGE_MAP()
-//
-//    // Что происходит когда надо отрисовать
-//    afx_msg void OnPaint();
-//
-//    // Данные о прямоугольнике, в котором нужно рисовать
-//    CRect rc;
-//};
-//
